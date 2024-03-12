a17e59912d6d5b8d771310ffb1b1a1909ffc6158
wolf606 <luismsanchez808@gmail.com>
2024-03-12 17:58:21 -0400

    Initial Commit

---
+++ game/gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameApp.swift
+//  game
+//
+//  Created by WOLF on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
